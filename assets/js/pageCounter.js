909654dec70f7d4752619fa8b7d78573424e5d1d
purvjeja <jejapurv@gmail.com>
2023-06-17 17:58:58 +0530

    Added admin view of page Count

--- assets/js/pageCounter.js
+++ assets/js/pageCounter.js
@@ -1,15 +1,18 @@
 const updateCountOf = async (page) => {
-    let dataItem = await fetch('https://api.jsonbin.io/v3/b/648c7bafb89b1e2299b0073f', {
-        headers: { "X-Master-Key": "$2b$10$UEaV7eg5gCdU0ZZPBl/GYuwdNtiBXxrGbbU4zfw4tcHbC3ipdy4KO"}      
-    }).then(res => res.json());
-    
-    dataItem = dataItem['record']
-    dataItem[page] =+ 1; 
+    if(true) 
+    {    
+        let dataItem = await fetch('https://api.jsonbin.io/v3/b/648c7bafb89b1e2299b0073f', {
+            headers: { "X-Master-Key": "$2b$10$UEaV7eg5gCdU0ZZPBl/GYuwdNtiBXxrGbbU4zfw4tcHbC3ipdy4KO"}      
+        }).then(res => res.json());
+        
+        dataItem = dataItem['record']
+        dataItem[page] += 1; 
 
-    const data = await fetch('https://api.jsonbin.io/v3/b/648c7bafb89b1e2299b0073f', {
-        method: "PUT",
-        headers: { "X-Master-Key": "$2b$10$UEaV7eg5gCdU0ZZPBl/GYuwdNtiBXxrGbbU4zfw4tcHbC3ipdy4KO", "Content-Type": "application/json" },
-        body: JSON.stringify(dataItem)  
-    }).then(res => res.json());
+        const data = await fetch('https://api.jsonbin.io/v3/b/648c7bafb89b1e2299b0073f', {
+            method: "PUT",
+            headers: { "X-Master-Key": "$2b$10$UEaV7eg5gCdU0ZZPBl/GYuwdNtiBXxrGbbU4zfw4tcHbC3ipdy4KO", "Content-Type": "application/json" },
+            body: JSON.stringify(dataItem)  
+        }).then(res => res.json());
+    }
 }
-
+ 
